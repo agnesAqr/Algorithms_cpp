aa939d7f1c559ba72d7ce7a9205282f11e2e2066
suhyun <86099781+agnesAqr@users.noreply.github.com>
2026-01-26 11:59:17 +0900

    solve by union-find

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,39 +1,67 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool visited[205];
-
-void bfs(const vector<vector<int>>& computers, int startNode)
+struct UnionFind
 {
-    queue<int> q;
-    q.push(startNode);
-    visited[startNode] = true;
+    vector<int> parent;
+    vector<int> rank;
     
-    while(!q.empty())
+    UnionFind(int n) : parent(n+1), rank(n+1, 0)
     {
-        int currComputer = q.front();
-        q.pop();
+        iota(parent.begin(), parent.end(), 0);
+    }
+    
+    int find(int x)
+    {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    
+    void unite(int x, int y)
+    {
+        int rootX = find(x);
+        int rootY = find(y);
         
-        for (int i=0; i<computers.size(); ++i)
+        if (rootX != rootY)
         {
-            if (currComputer == i || computers[currComputer][i] == 0 || visited[i]) continue;
-            
-            visited[i] = true;
-            q.push(i);
+            if (rank[rootX] < rank[rootY])
+                parent[rootX] = rootY;
+            else
+            {
+                parent[rootY] = rootX;
+                if (rank[rootX] == rank[rootY])
+                    rank[rootX]++;
+            }
         }
     }
-}
     
-int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
+    bool same(int x, int y)
+    {
+        return find(x) == find(y);
+    }
+};
+  
+
+int solution(int n, vector<vector<int>> computers)
+{
+    int answer{};
+    
+    UnionFind uf(n);
+    for (int i=0; i<n; ++i)
+    {
+        for (int j=i+1; j<n; ++j)
+        {
+            if (i != j && computers[i][j] == 1)
+                uf.unite(i, j);
+        }
+    }
     
     for (int i=0; i<n; ++i)
     {
-        if (!visited[i])
-        {
-            bfs(computers, i);
+        if (uf.parent[i] == i)
             answer++;
-        }
     }
+    
     return answer;
 }
