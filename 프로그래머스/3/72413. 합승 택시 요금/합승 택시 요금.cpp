8d378c00ae84249807c823c14d578bb4d7b47501
suhyun <86099781+agnesAqr@users.noreply.github.com>
2026-02-10 17:51:13 +0900

    Refactor taxi fare calculation algorithm

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,55 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int MAXNODE = 200;
 constexpr int INF = 1e9;
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-    vector<vector<pair<int, int>>> graph(n+1);
+    vector<vector<int>> dist(n+1, vector<int>(n+1, INF));
+    for (int i=1; i<=n; ++i) dist[i][i] = 0;
+    
     for (const auto& f : fares)
     {
-        graph[f[0]].emplace_back(f[2], f[1]);
-        graph[f[1]].emplace_back(f[2], f[0]);
+        dist[f[0]][f[1]] = f[2];
+        dist[f[1]][f[0]] = f[2];
     }
     
-    auto dijkstra = [&](int start) -> vector<int>{
-        vector<int> dist(n+1, INF);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        pq.emplace(0, start);
-        dist[start] = 0;
-        
-        while (!pq.empty())
+    for (int k=1; k<=n; ++k)
+    {
+        for (int i=1; i<=n; ++i)
         {
-            auto [cost, node] = pq.top();
-            pq.pop();
-            
-            if (cost > dist[node]) continue;
-            
-            for (const auto& [nextCost, nextNode] : graph[node])
+            for (int j=1; j<=n; ++j)
             {
-                int newCost = cost + nextCost;
-                if (newCost < dist[nextNode])
+                if (dist[i][k] != INF && dist[k][j] != INF)
                 {
-                    dist[nextNode] = newCost;
-                    pq.emplace(newCost, nextNode);
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
-        return dist;
-    };
-    
-    vector<int> distS = dijkstra(s);
-    vector<int> distA = dijkstra(a);  // A -> K (== K -> A)
-    vector<int> distB = dijkstra(b);  // B -> K (== K -> B)
-    
+    }
     int answer = INF;
-    for (int k=1; k<=n; ++k)
-    {
-        if (distS[k] != INF && distA[k] != INF && distB[k] != INF)
-        {
-            answer = min(answer, distS[k] + distA[k] + distB[k]);
+    for (int k = 1; k <= n; k++) {
+        if (dist[s][k] != INF && dist[k][a] != INF && dist[k][b] != INF) {
+            answer = min(answer, dist[s][k] + dist[k][a] + dist[k][b]);
         }
     }
-    
     return answer;
 }
