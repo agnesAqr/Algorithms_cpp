b365717f28333fd3619c8341a727791336243c9b
suhyun <86099781+agnesAqr@users.noreply.github.com>
2026-02-11 14:53:56 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class UnionFind
+{
+private:
+    vector<int> parent{};
+    vector<int> rank{};
+
+public:
+    UnionFind(int n)
+    {
+        parent.resize(n+1);
+        iota(parent.begin(), parent.end(), 0);
+        rank.resize(n+1, 0);
+    }
+
+    int find(int x)
+    {
+        if (parent[x] == x)
+            return x;
+        
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int x, int y)
+    {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+
+        if (rank[rootX] > rank[rootY])
+        {
+            parent[rootY] = rootX;
+        }
+        else if (rank[rootX] < rank[rootY])
+        {
+            parent[rootX] = rootY;
+        }
+        else
+        {
+            parent[rootX] = rootY;
+            rank[rootY]++;
+        }
+    }
+
+    bool isConnected(int x, int y)
+    {
+        return find(x) == find(y);
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N{}, M{};
+    cin >> N >> M;
+
+    UnionFind uf(N);
+    for (int i=1; i<=N; ++i)
+    {
+        for (int j=1; j<=N; ++j)
+        {
+            int isConnected{};
+            cin >> isConnected;
+            if (isConnected == 1)
+                uf.unite(i, j);
+        }
+    }
+
+    bool possible = true;
+    vector<int> travelRoute(M, 0);
+    for (int i=0; i<M; ++i)
+    {
+        int travelNode{};
+        cin >> travelNode;
+        travelRoute[i] = travelNode;
+        if (i > 0)
+        {
+            possible &= uf.isConnected(travelRoute[i-1], travelRoute[i]);
+        }
+    }
+    cout << (possible ? "YES" : "NO");
+
+	return 0;
+}
