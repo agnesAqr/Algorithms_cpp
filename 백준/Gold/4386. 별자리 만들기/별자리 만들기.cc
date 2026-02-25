e86f22d8cdcd803ad5459f876c185e0b1c8d7610
suhyun <86099781+agnesAqr@users.noreply.github.com>
2026-02-25 13:00:25 +0900

    [Gold III] Title: 별자리 만들기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
@@ -2,45 +2,6 @@
 using namespace std;
 using pff = pair<float, float>;
 
-struct Edge
-{
-    int u{}, v{};
-    float w{};
-    Edge(int u, int v, float w) : u(u), v(v), w(w){};
-    bool operator<(const Edge& other) const { return w < other.w; }
-};
-
-class UnionFind
-{
-private:
-    vector<int> parent, rank;
-public:
-    UnionFind(int n) : parent(n), rank(n, 0)
-    {
-        iota(parent.begin(), parent.end(), 0);
-    }
-    int find(int x)
-    {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    bool unite(int x, int y)
-    {
-        x = find(x);
-        y = find(y);
-        if (x == y) return false;
-
-        if (rank[x] > rank[y]) parent[y] = x;
-        else if (rank[y] > rank[x]) parent[x] = y;
-        else
-        {
-            parent[y] = x;
-            rank[x]++;
-        }
-        return true;
-    }
-};
-
 float CalculateCost(pff s1, pff s2)
 {
     return hypot(s2.first - s1.first, s2.second - s1.second);
@@ -60,32 +21,33 @@ int main()
         cin >> stars[i].first >> stars[i].second;
     }
 
-    vector<Edge> edges;
-    edges.reserve(n*(n-1)/2);
+    vector<bool> visited(n, false);
+    vector<float> minDist(n, 2000.0f);
+    float mst_weight{};
+    minDist[0] = 0.0f;
+
     for (int i=0; i<n; ++i)
     {
-        for (int j=i+1; j<n; ++j)
+        int u = -1;
+        for (int j=0; j<n; ++j)
         {
-            if (j == i) continue;
-            float dist = CalculateCost(stars[i], stars[j]);
-            edges.emplace_back(i, j, dist);
+            if (!visited[j] && (u == -1 || minDist[j] < minDist[u]))
+                u = j;
         }
-    }
-    sort(edges.begin(), edges.end());
+                
+        visited[u] = true;
+        mst_weight += minDist[u];
 
-    UnionFind uf(n);
-    float mst_weight{};
-    int mst_edgeCount{};
-    for (const auto& [u, v, w] : edges)
-    {
-        if (uf.unite(u, v))
+        for (int v=0; v<n; ++v)
         {
-            mst_weight += w;
-            if (++mst_edgeCount == n-1)
-                break;
+            if (!visited[v])
+            {
+                float dist = CalculateCost(stars[u], stars[v]);
+                if (dist < minDist[v])
+                    minDist[v] = dist;
+            }
         }
     }
     cout << fixed << setprecision(2) << mst_weight << "\n";
-    
 	return 0;
 }
