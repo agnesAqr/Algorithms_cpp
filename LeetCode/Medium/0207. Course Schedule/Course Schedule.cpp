01210a49827f9fed7d58270e42410e85a2fa8a8e
suhyun <86099781+agnesAqr@users.noreply.github.com>
2026-03-05 15:22:38 +0900

    [LeetCode] 207. Course Schedule (C++)

--- LeetCode/Medium/0207. Course Schedule/Course Schedule.cpp
+++ LeetCode/Medium/0207. Course Schedule/Course Schedule.cpp
@@ -1,21 +1,26 @@
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌rank[y]++;
-·‌·‌·‌·‌·‌·‌·‌·‌}
-·‌·‌·‌·‌·‌·‌·‌·‌return·‌true;
-·‌·‌·‌·‌}
-};
+            rank[y]++;
+        else if (rank[y] > rank[x]) parent[x] = y;
+        else
+        {
+            parent[x] = y;
+        if (x == y) return false;
 
-class·‌Solution·‌{
+        if (rank[x] > rank[y]) parent[y] = x;
+    bool unite(int x, int y)
+    {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+        iota(parent.begin(), parent.end(), 0);
+    }
+    int find(int x)
+    {
 public:
-·‌·‌·‌·‌bool·‌canFinish(int·‌numCourses,·‌vector<vector<int>>&·‌prerequisites)·‌{
-·‌·‌·‌·‌·‌·‌·‌·‌UnionFind·‌uf(numCourses);
-·‌·‌·‌·‌·‌·‌·‌·‌for·‌(const·‌auto&·‌p·‌:·‌prerequisites)
-·‌·‌·‌·‌·‌·‌·‌·‌{
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌int·‌pre·‌=·‌p[1];
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌int·‌next·‌=·‌p[0];
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌if·‌(uf.find(pre)·‌==·‌next)
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌return·‌false;
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌uf.unite(next,·‌pre);
-·‌·‌·‌·‌·‌·‌·‌·‌}
-·‌·‌·‌·‌·‌·‌·‌·‌return·‌true;
-·‌·‌·‌·‌}
-};
+    UnionFind(int n) : parent(n+1), rank(n+1, 0)
+    {
+{
+private:
+    vector<int> parent;
+    vector<int> rank;
+        }
+        return true;
