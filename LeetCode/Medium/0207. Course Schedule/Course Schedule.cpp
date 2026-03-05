e251eae5ce854b5c2b53126a4165f90850085ea7
suhyun <86099781+agnesAqr@users.noreply.github.com>
2026-03-05 15:11:54 +0900

    [LeetCode] 207. Course Schedule (C++)

---
+++ LeetCode/Medium/0207. Course Schedule/Course Schedule.cpp
@@ -0,0 +1,24 @@
+        else
+        {
+            parent[x] = y;
+            rank[y]++;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        UnionFind uf(numCourses);
+        for (const auto& p : prerequisites)
+        {
+            int pre = p[1];
+            int next = p[0];
+            if (uf.find(pre) == next)
+                return false;
+            uf.unite(next, pre);
+        }
+        return true;
+    }
+};
---
+++ LeetCode/Medium/0207. Course Schedule/README.md
@@ -0,0 +1,15 @@
+# [207. Course Schedule](https://leetcode.com/problems/course-schedule/) 🟡
+
+## Difficulty
+
+🟡 **Medium**
+
+## Performance
+
+Runtime: N/A
+
+Memory: N/A
+
+---
+
+> Uploaded by CodeHub
