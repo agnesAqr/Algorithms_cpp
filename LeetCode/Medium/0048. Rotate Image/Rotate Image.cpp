61bdbd137f004c136fe2914eca48af55a1187508
suhyun <86099781+agnesAqr@users.noreply.github.com>
2026-03-09 14:06:54 +0900

    [LeetCode] 48. Rotate Image (C++)

--- LeetCode/Medium/0048. Rotate Image/Rotate Image.cpp
+++ LeetCode/Medium/0048. Rotate Image/Rotate Image.cpp
@@ -1,18 +1,25 @@
-            {
-                for (int i=0; i<4; ++i)
-                {
-                    swap(cr, cc);
-                int cr = currRow;
-                int cc = c;
-                    cc = n-1-cc;
-                    int moveValue = prev;
-                int prev = matrix[cr][cc];
+class Solution {
+public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int turn = n/2;
-        vector<int> memory(4, 0);
         for (int i=1; i<=turn; ++i)
         {
             int currRow = i-1;
             for (int c=currRow; c<n-i; ++c)
-public:
+            {
+                int cr = currRow;
+                int cc = c;
+                int prev = matrix[cr][cc];
+                for (int i=0; i<4; ++i)
+                {
+                    int moveValue = prev;
+                    swap(cr, cc);
+                    cc = n-1-cc;
+                    prev = matrix[cr][cc];
+                    matrix[cr][cc] = moveValue;
+                }
+            }
+        }
+    }
+};
